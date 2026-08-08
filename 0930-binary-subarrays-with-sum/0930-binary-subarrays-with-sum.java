ad46fa855f4e789153a6a30e66402a84717dd30a
Piyush Sharma <piyushsharma.cs23@bmsce.ac.in>
2026-08-08 09:58:28 +0530

    Time: 23 ms (33.51%), Space: 52.5 MB (24.02%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,35 +1,17 @@
-class Solution {
-
+class Solution{
     public int numSubarraysWithSum(int[] nums, int goal) {
-
-        return atMost(nums, goal) - atMost(nums, goal - 1);
-    }
-
-    private int atMost(int[] nums, int goal) {
-
-        if (goal < 0)
-            return 0;
-
-        int left = 0;
-        int right = 0;
-        int sum = 0;
+        HashMap<Integer,Integer>map = new HashMap<>();
+        map.put(0,1);
+        int prefixsum = 0;
         int count = 0;
-
-        while (right < nums.length) {
-
-            sum += nums[right];
-
-            while (sum > goal) {
-
-                sum -= nums[left];
-                left++;
+        for(int num:nums){
+            prefixsum += num;
+            int previousPrefix = prefixsum - goal;
+            if(map.containsKey(previousPrefix)){
+                count += map.get(previousPrefix);
             }
-
-            count += right - left + 1;
-
-            right++;
+            map.put(prefixsum,map.getOrDefault(prefixsum,0)+1);
         }
-
         return count;
     }
 }
