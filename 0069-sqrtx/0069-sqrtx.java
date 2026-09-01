dbfb29d7743425864debddd1b18f09812a71c2ac
Piyush Sharma <piyushsharma.cs23@bmsce.ac.in>
2026-09-01 05:59:36 +0530

    Time: 1 ms (98.72%), Space: 42.5 MB (61.69%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x<2){
+            return x;
+        }
+        int low = 1;
+        int high = x;
+        while(low <= high){
+            int mid = low +(high - low)/2;
+            if ((long) mid * mid <= x){
+                low = mid +1;
+            } else {
+                high = mid -1;
+            }
+        }
+        return high;
+    }
+}
